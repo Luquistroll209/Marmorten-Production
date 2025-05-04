eca68187bbff210a76f9399a810256f4e3d9a4e7
LT-209 <94721500+Luquistroll209@users.noreply.github.com>
2025-05-04 08:50:59 +0200

    sin http

--- marmorten_production/marmorten_production/settings.py
+++ marmorten_production/marmorten_production/settings.py
@@ -142,6 +142,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://story-lee-harry-meal.trycloudflare.com/',
 ]
 
-ALLOWED_HOSTS = ['https://story-lee-harry-meal.trycloudflare.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['story-lee-harry-meal.trycloudflare.com', 'localhost', '127.0.0.1']
+
 
 CORS_ALLOWED_ORIGINS = ["https://story-lee-harry-meal.trycloudflare.com/"]
