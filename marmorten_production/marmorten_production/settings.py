956cf8623bb45d0079b166b6a633e2b05145b082
LT-209 <94721500+Luquistroll209@users.noreply.github.com>
2025-05-04 08:44:27 +0200

    cambio de url

--- marmorten_production/marmorten_production/settings.py
+++ marmorten_production/marmorten_production/settings.py
@@ -139,11 +139,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Tema de dominios
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://armenia-launched-coupons-tuning.trycloudflare.com',
+    'https://story-lee-harry-meal.trycloudflare.com/',
 ]
 
-ALLOWED_HOSTS = ['armenia-launched-coupons-tuning.trycloudflare.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://story-lee-harry-meal.trycloudflare.com/', 'localhost', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
-    "https://armenia-launched-coupons-tuning.trycloudflare.com",
+    "https://story-lee-harry-meal.trycloudflare.com/",
 ]
