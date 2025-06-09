226e57fb73ee19d0b4a5864c5e9540e7b71c6718
LT-209 <luquistroll@gmail.com>
2025-06-09 23:27:06 +0200

    false

--- marmorten_production/marmorten_production/settings.py
+++ marmorten_production/marmorten_production/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bh4c+q0w8r7gp%&f!l4+-l6ggf6led$sm3%jwz56i@ned@q!-6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 handler404 = 'blog.views.custom_404'
 
 
